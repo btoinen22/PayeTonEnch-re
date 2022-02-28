510e21a8064c8f0b7917f55488157fe5a9578f56
emilie-lh <78213650+emilie-lh@users.noreply.github.com>
2022-02-28 11:41:25 +0100

    class encherir

--- PayeTonEnchère/PayeTonEnchère/models/Encherir.cs
+++ PayeTonEnchère/PayeTonEnchère/models/Encherir.cs
@@ -4,15 +4,29 @@ using System.Text;
 
 namespace PayeTonEnchère.models
 {
-    internal class Encherir
+    public class Encherir
     {
-		//
-		// TODO: Add constructor logic here
-		//
+	
 		#region Attribut
+
+		private int _id;
+		private Enchere _laenchere_id;
+		private User _leuser_id;
+		private double _prixenchere;
+		private DateTime _dateenchere;
+
+
 		#endregion
 
 		#region Constructeur
+		public Encherir(int id,double prixenchere,DateTime dateenchere,Enchere laenchere_id, User leuser_id)
+        {
+			_id= id;
+			_laenchere_id= laenchere_id;
+			_dateenchere= dateenchere;
+			_prixenchere= prixenchere;
+			this._leuser_id= leuser_id;
+        }
 		#endregion
 
 		#region Getters/Setters
