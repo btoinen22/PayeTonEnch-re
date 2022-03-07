bd3ac4f3c2d008b1f4e71eb0a7c36a8eb20015b4
emilie-lh <78213650+emilie-lh@users.noreply.github.com>
2022-03-07 08:27:03 +0100

    ok

--- PayeTonEnchère/PayeTonEnchère/Constantes.cs
+++ PayeTonEnchère/PayeTonEnchère/Constantes.cs
@@ -8,7 +8,7 @@ namespace PayeTonEnchère
     {
         /* adresse de l'API au lycée à lancée avec un VPN ou en étant connecté
         // sur le réseau du lycée sur le broadcast de la box '172.17.255.253' */
-        public static string APIenchere = 172.17.0.61:8000;
+        public static string APIenchere = "172.17.0.61:8000";
 
         /* adresse de l'API local
         // public static string APIenchereNo = 10.10.0.22:8000; */
