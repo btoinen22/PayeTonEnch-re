48700354e3187e9451c9a2baf59ae6502fd0caf6
emilie-lh <78213650+emilie-lh@users.noreply.github.com>
2022-03-22 16:44:01 +0100

    ok

---
+++ PayeTonEnchère/PayeTonEnchère/VueModels/AccueilVueModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayeTonEnchère.VueModels
+{
+    public class AccueilVueModels
+    {
+        #region attributs
+        // attribut qui permet de rendre ou non visible un éléments dans la vue correspondante
+        public bool IsVisible { get; }
+
+        #endregion
+    }
+}
