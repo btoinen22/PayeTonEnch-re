d5ab42ec6ffa7352205bc422b6f0ab6fc09ffe9f
btoinen22 <75849304+btoinen22@users.noreply.github.com>
2022-03-29 08:19:06 +0200

    ok

--- PayeTonEnchère/PayeTonEnchère/Constantes.cs
+++ PayeTonEnchère/PayeTonEnchère/Constantes.cs
@@ -9,7 +9,8 @@ namespace PayeTonEnchère
         /* adresse de l'API au lycée à lancée avec un VPN ou en étant connecté
         // sur le réseau du lycée sur le broadcast de la box '172.17.255.253' */
         public static string 
-            //APIenchere = "http://172.17.0.61:8000/api/",
+            //APIenchere = "http://172.17.0.61:80
+            //00/api/",
             //Adresse de l'API en serveur distant
             APIenchere = "http://80.13.113.244:2081/api/"
             ;
